a41780af619743845d08253dfe630c68cf7a4a3e
Sudarshan Pardeshi <sudarshanpardeshi71@outlook.com>
2026-06-01 10:54:36 +0000

    created app and basic info

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import joblib
+import keras
+import pandas as pd
+
+# Provide Tab title
+st.set_page_config("Deployment Project")
+
+# Provide the page title
+st.title("Booking Status Prediction Model")
+
+#Mention description of the problem statement or if you want to mention your 
+st.subheader("By Sudarshan Pardeshi")
